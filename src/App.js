60db2fa96e7e79a4ca6fe86762f6214425bdbd4a
chalitgubkb <birdbkbbird@gmail.com>
2018-09-29 06:49:49 +0700

    เริ่ม Commit ส่วน index.html ใหม่ Commit ของเก่าหมายหมด

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WordCard from './WordCard.js';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <WordCard value="HELLO"/>
+    </div>
+    );
+  }
+}
+
+export default App;
