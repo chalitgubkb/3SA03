b351e27e227a2b7bf6f686c43d5e9eba8ab572e7
chalitgubkb <birdbkbbird@gmail.com>
2018-09-29 08:03:31 +0700

    Create file style.js

--- src/WordCard.js
+++ src/WordCard.js
@@ -16,9 +16,7 @@ const prepareStateFromWord = (given_word) => {
 export default class WordCard extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            attempt: '',
-        }
+        this.state = {}
     }
     
     componentWillMount(){
@@ -32,15 +30,28 @@ export default class WordCard extends Component {
         })
     }
 
-
-    activationHandler = c => { 
-        console.log(`${c} has been activated.`) 
+    activationHandler = (c) => {
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        if(guess.length == this.state.chars.length){
+            if(this.state.word.match(guess.join('').toString())){
+                this.setState({
+                    guess: [], 
+                    completed: true})
+            }else{
+                this.setState({
+                    guess: [], 
+                    attempt: this.state.attempt + 1
+                })
+            }
+        }
     }
-    
+
     render() {
+        console.log(this.state);
         return (
             <div >
-                { Array.from(this.props.value).map((c, i) => 
+                { this.state.chars.map((c, i) => 
                     <CharacterCard value={c} key={i} attempt={this.state.attempt}
                     activationHandler={this.activationHandler}/>                    
                 )}
