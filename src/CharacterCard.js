4b93fc9c3db81f8e93299d87eae4d440e82f9672
chalitgubkb <birdbkbbird@gmail.com>
2018-09-29 07:04:29 +0700

    change CharacterCard has Component

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -3,32 +3,24 @@ import './App.css';
 
 
 export default class CharacterCard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            active: false,
-        }
+    constructor(props){
+    super(props)
+    this.state = {
+    active: false,
     }
-    
+    }
+   
     activate = () => {
-        if(!this.state.active){
-            this.props.activationHandler(this.props.value)
-            this.setState({active: true})
-        }
+    if(!this.state.active){
+    this.setState({active: true, seq})
     }
-
-    componentDidUpdate(prevProps){
-        if(prevProps.attempt != this.props.attempt){
-        this.setState({active: false})
-        }
     }
-       
-    render() {
-        let className = `card ${this.state.active ? 'activeCard' : ''}`
-        return (
-            <div className={className} onClick={this.activate}>
-                {this.props.value}
-            </div>
-        )
+    render(){
+    let className = `card ${this.state.active ? 'activeCard': ''}`
+    return (
+    <div className={className} onClick={this.activate}>
+    {this.props.value}
+    </div>
+    )
     }
-}
+   }
