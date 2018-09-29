5980ff52653550157c8232e14e06be24c5caa274
chalitgubkb <birdbkbbird@gmail.com>
2018-09-29 20:06:02 +0700

    เปลี่ยนสี menubar ด้านบนสุด

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CharacterCard from './CharacterCard.js';
 import _ from 'lodash';
+
 const prepareStateFromWord = (given_word) => {
  let word = given_word.toUpperCase()
  let chars = _.shuffle(Array.from(word))
